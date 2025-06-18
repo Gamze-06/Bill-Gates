09862d1b634c52e1abcf9c69fb61f539808c1290
Gamze <gamzegoren35@hotmail.com>
2025-06-18 17:04:44 +0300

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,86 @@
+const products = [
+    {
+        id: 1,
+        name: "Ada",
+        price: 200000000,
+        image: "/assets/ada.png",
+    },
+
+    {
+        id: 2,
+        name: "Araba",
+        price: 50000,
+        image: "/assets/araba.png",
+    },
+
+    {
+        id: 3,
+        name: "Araba2",
+        price: 80000,
+        image: "/assets/araba_.png",
+    },
+
+    {
+        id: 4,
+        name: "Feribot",
+        price: 400000000,
+        image: "/assets/feribot.png",
+    },
+
+    {
+        id: 5,
+        name: "Gemi",
+        price: 9000000,
+        image: "/assets/gemi.png",
+    },
+
+    {
+        id: 6,
+        name: "Hamburger",
+        price: 10000,
+        image: "/assets/hamburger.png",
+    },
+
+    {
+        id: 7,
+        name: "Helikopter",
+        price: 90000000,
+        image: "/assets/helikopter.png",
+    },
+    {
+        id: 8,
+        name: "Tren",
+        price: 60000000,
+        image: "/assets/tren.png",
+    },
+
+     {
+        id: 9,
+        name: "Uçak",
+        price: 60000000,
+        image: "/assets/ucak.png",
+    },
+
+     {
+        id: 10,
+        name: "Yat",
+        price: 100000000,
+        image: "/assets/yat.png",
+    },
+
+     {
+        id: 11,
+        name: "Yemek",
+        price: 30000,
+        image: "/assets/yemek-.png",
+    },
+
+     {
+        id: 12,
+        name: "Yemek2",
+        price: 60000000,
+        image: "/assets/yemek.png",
+    },
+];
+
+export default products;
